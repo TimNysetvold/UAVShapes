1e3ad4126bb97d3af28af120bc3ef0dbf98ec691
TimNysetvold <tim.nysetvold@gmail.com>
2019-03-27 19:02:53 -0600

    Uploading from previous file management

---
+++ AltDetLayers.m
@@ -0,0 +1,12 @@
+function [droneCruiseHeight] = AltDetLayers(objective,dronePosition,layerMat)
+%A simple layering method for determining what layer drones should fly on.
+    numLayers=length(layerMat);
+    degreesPerLayer=360/numLayers;
+    heading=getHeading(objective,dronePosition);
+    angle=headingtoAngle(heading);
+    %This line assigns a layer to the drone based on its heading.
+    %By dividing the heading by the number of degrees per layer,
+    %and rounding the result up, we can assign a layer between 1
+    %and numLayers.
+    droneCruiseHeight=layerMat(ceil(angle/degreesPerLayer));
+end
